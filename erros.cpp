4975cf28a9ac7fc4c8c41a28010a192586576d77
Geraldo <eng.geraldo.braz@gmail.com>
2016-09-12 23:42:57 -0300

    Primeiro commit

---
+++ erros.cpp
@@ -0,0 +1,13 @@
+#include "erros.h"
+
+erros::erros(){
+
+}
+
+double erros::erroAbs(double xa, x){
+	return abs(xa - x);
+}
+
+double erros::erroRel(double xa, x){
+	return abs(xa - x)/abs(x);
+}
---
+++ erros.h
@@ -0,0 +1,8 @@
+class erros{
+
+public:
+	erros();
+	double erroAbs(double xa, double x);
+	double erroRel(double xa, double x);
+
+};
