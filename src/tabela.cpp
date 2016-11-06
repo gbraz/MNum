32da9ce2079496ecacd522c6d52c85bb9a61e8bd
Geraldo <eng.geraldo.braz@gmail.com>
2016-11-06 13:17:06 -0300

    comentários

--- src/tabela.cpp
+++ src/tabela.cpp
@@ -4,15 +4,18 @@ Tabela::Tabela(){
 
 }
 
-void Tabela::imprimirResultados(std::vector<std::vector<double> > v1, std::vector<std::vector<double> > v2){
+/*Imprime uma tabela para as iterações de Newton-Raphson e outra para Secante
+  Entradas: std::vector<std::vector<double> > m1 - matriz dos resultados de Newton-Raphson
+  			std::vector<std::vector<double> > m2 - matriz dos resultados do método da Secante*/
+void Tabela::imprimirResultados(std::vector<std::vector<double> > m1, std::vector<std::vector<double> > m2){
 	
 	system("clear");
 	std::cout << "+----------------Newton-Raphson---------------------+" << std::endl; //Imprime tabela pro método de NR
 	std::cout << "+---+---------------+---------------+---------------+" << std::endl;
 	std::cout << "| k |      xk       |      f(xk)    |      f'(xk)   |" << std::endl;
 	std::cout << "+---+---------------+---------------+---------------+" << std::endl;
-	for(int i = 0; i < v1.size(); i++)	
-		printf("|%3d|%15.10f|%15.10f|%15.10f|\n", i, v1[i][0], v1[i][1], v1[i][2]);
+	for(int i = 0; i < m1.size(); i++)	
+		printf("|%3d|%15.10f|%15.10f|%15.10f|\n", i, m1[i][0], m1[i][1], m1[i][2]);
 	std::cout << "+---+---------------+---------------+---------------+" << std::endl;
 	std::cout << std::endl << std::endl;;
 	
@@ -20,7 +23,7 @@ void Tabela::imprimirResultados(std::vector<std::vector<double> > v1, std::vecto
 	std::cout << "+---+---------------+---------------+" << std::endl;
 	std::cout << "| k |      xk       |     f(xk)     |" << std::endl;
 	std::cout << "+---+---------------+---------------+" << std::endl;
-	for(int i = 0; i < v2.size(); i++)	
-		printf("|%3d|%15.10f|%15.10f|\n", i, v2[i][0], v2[i][1]);
+	for(int i = 0; i < m2.size(); i++)	
+		printf("|%3d|%15.10f|%15.10f|\n", i, m2[i][0], m2[i][1]);
 	std::cout << "+---+---------------+---------------+" << std::endl;
 }
